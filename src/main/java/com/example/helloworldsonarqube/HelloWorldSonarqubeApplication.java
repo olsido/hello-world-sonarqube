d5943343a0d0fea23e42d935de5ad6976c12a34a
Olga Strijewski <Olga.Strijewski@smithcommerce.com>
2024-07-07 20:13:22 -0400

    Initial commit with Spring Boot project setup

---
+++ src/main/java/com/example/helloworldsonarqube/HelloWorldSonarqubeApplication.java
@@ -0,0 +1,13 @@
+package com.example.helloworldsonarqube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldSonarqubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldSonarqubeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/helloworldsonarqube/HelloWorldSonarqubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloworldsonarqube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldSonarqubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
