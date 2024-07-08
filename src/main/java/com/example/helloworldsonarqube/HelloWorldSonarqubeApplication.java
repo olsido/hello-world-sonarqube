e70ac39b701b8c95cc41082bdec974eeb5311c1b
Olga Strijewski <Olga.Strijewski@smithcommerce.com>
2024-07-08 00:03:47 -0400

    Added a controler and tests

--- src/main/java/com/example/helloworldsonarqube/HelloWorldSonarqubeApplication.java
+++ src/main/java/com/example/helloworldsonarqube/HelloWorldSonarqubeApplication.java
@@ -2,12 +2,22 @@ package com.example.helloworldsonarqube;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class HelloWorldSonarqubeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldSonarqubeApplication.class, args);
+	}
+
+	@RestController
+	class HelloWorldController {
+		@GetMapping("/")
+		public String hello() {
+			return "Hello, World!";
+		}
 	}
 
 }
--- src/test/java/com/example/helloworldsonarqube/HelloWorldSonarqubeApplicationTests.java
+++ src/test/java/com/example/helloworldsonarqube/HelloWorldSonarqubeApplicationTests.java
@@ -3,11 +3,22 @@ package com.example.helloworldsonarqube;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 class HelloWorldSonarqubeApplicationTests {
 
 	@Test
 	void contextLoads() {
+		HelloWorldSonarqubeApplication.HelloWorldController application = new HelloWorldSonarqubeApplication().new HelloWorldController();
+		assertThat(application).isNotNull();
+	}
+
+	@Test
+	void testHelloWorld() {
+		HelloWorldSonarqubeApplication.HelloWorldController controller = new HelloWorldSonarqubeApplication().new HelloWorldController();
+		String response = controller.hello();
+		assertThat(response).isEqualTo("Hello, World!");
 	}
 
 }
